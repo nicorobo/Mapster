b3c01cf560e721d113ff8306c6fb37c1aad2a1d0
Nick Roberts <nickroberts404@gmail.com>
2014-12-27 15:02:08 -0600

    Added functionality to map controls using Mapster object.

---
+++ js/main.js
@@ -0,0 +1,46 @@
+var Mapster = function(target){
+	this.target = target;
+	this.matrix = [1, 0, 0, 1, 0, 0];
+	this.width = $('#map-container').width();
+	this.height = $('#map-container').height();
+	console.log(this.width);
+}
+
+Mapster.prototype = {
+	pan: function(dx, dy){
+		this.matrix[4]+= dx;
+		this.matrix[5]+= dy;
+		this.apply();
+	},
+	zoom: function(factor){
+		for(var i=0; i<this.matrix.length; i++){
+			this.matrix[i]*=factor;
+		}
+		this.matrix[4]+= (1-factor)*this.width/2;
+		this.matrix[5]+= (1-factor)*this.height/2;
+		this.apply();
+	},
+	reset: function(){
+		this.matrix = [1, 0, 0, 1, 0, 0];
+		this.apply();
+	},
+	apply: function(){
+		var newTransformation = 'matrix('+this.matrix.join(' ')+')';
+		$(this.target).attr('transform', newTransformation);
+	}
+}
+
+var map = new Mapster('#map-matrix');
+
+$('#controls').on('click', '.control', changeMap);
+
+function changeMap(){
+	var id = $(this).attr('id');
+	if(id == 'reset') map.reset();
+	if(id == 'zoomIn') map.zoom(1.5);
+	if(id == 'zoomOut') map.zoom(0.5);
+	if(id == 'up') map.pan(0, 30);
+	if(id == 'down') map.pan(0, -30);
+	if(id == 'left') map.pan(30, 0);
+	if(id == 'right') map.pan(-30, 0);
+}

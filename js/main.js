0092e49bd4a5b86974e8017554a856ca6faf61e0
Nick Roberts <nickroberts404@gmail.com>
2015-01-04 13:46:36 -0600

    Removed fontawesome, seperated array construction and grid drawing.

--- js/main.js
+++ js/main.js
@@ -7,7 +7,9 @@ function createMap(mapdata){
 	var Mapster = function(map, snap){
 		this.g = map;
 		this.s = snap;
+		this.mapArray = [];
 		this.gridSet = this.g.group();
+		this.$gridSquares = $('.grid-square');
 		this.bounding = this.g.getBBox();
 		this.width = this.bounding.width;
 		this.height = this.bounding.height;
@@ -17,40 +19,70 @@ function createMap(mapdata){
 		markCenter: function(){
 			var bounding = this.g.getBBox();
 			var center = this.g.circle(bounding.cx, bounding.cy, 5).attr({fill:'salmon'});
-			var line1 = this.g.line(bounding.x, bounding.y, bounding.x, bounding.y2);
-			var line2 = this.g.line(bounding.x2, bounding.y, bounding.x, bounding.y);
-			var line3 = this.g.line(bounding.x, bounding.y2, bounding.x2, bounding.y2);
-			var line4 = this.g.line(bounding.x2, bounding.y2, bounding.x2, bounding.y);
-			var square = this.g.group(line1, line2, line3, line4).attr({stroke:'salmon'});
+			// var line1 = this.g.line(bounding.x, bounding.y, bounding.x, bounding.y2);
+			// var line2 = this.g.line(bounding.x2, bounding.y, bounding.x, bounding.y);
+			// var line3 = this.g.line(bounding.x, bounding.y2, bounding.x2, bounding.y2);
+			// var line4 = this.g.line(bounding.x2, bounding.y2, bounding.x2, bounding.y);
+			// var square = this.g.group(line1, line2, line3, line4).attr({stroke:'salmon'});
 			return center;
 		},
-		drawGrid: function(num){
-			var squareWidth = this.width/num;
-			var numOfSquaresX = num;
+		loadArray: function(array){
+			this.mapArray = array;
+		},
+		createArray: function(size){
+			var yArray = [];
+			var squareWidth = this.width/size;
+			var numOfSquaresX = size;
 			var numOfSquaresY = Math.ceil(this.height/squareWidth);
+			for(var i=0; i<numOfSquaresY; i++){
+				var xArray = [];
+				for(var j=0; j<numOfSquaresX; j++){
+					xArray.push(1);
+				}
+				yArray.push(xArray);
+			}
+			this.mapArray = yArray;
+		},
+		drawGrid: function(){
+			var squareWidth = this.width/this.mapArray[0].length;
 			this.removeGrid();
-			// this.g.selectAll('.grid-square').remove();
-			// var p = new Snap();
-			// var squares = p.group()
 			var x=0;
 			var y=0;
-			for(var i=0; i<numOfSquaresX; i++){
-				y=0;
-				for(var j=0; j<numOfSquaresY; j++){
-					this.gridSet.rect(x, y, squareWidth, squareWidth).attr({class:"grid-square", id:"square"+j+'-'+i})
-					y+=squareWidth;
+			for(var i=0; i<this.mapArray.length; i++){
+				x=0;
+				for(var j=0; j<this.mapArray[i].length; j++){
+					this.gridSet.rect(x, y, squareWidth, squareWidth).attr({class:"grid-square type"+this.mapArray[i][j], id:"square"+j+'-'+i})
+					x+=squareWidth;
 				}
-				x+=squareWidth;
+				y+=squareWidth;
 			}
-			// this.g.append(squares);
-		}, 
+			this.$gridSquares = $('.grid-square');
+			this.$gridSquares.on('mouseover', readSquare);
+			function readSquare(){
+				var coords =  $(this).attr('id').slice(6).split('-');
+				$('#squareX').text(parseInt(coords[0])+1);
+				$('#squareY').text(parseInt(coords[1])+1);
+			}
+		},
+		colorSquare: function(squares){
+			console.log(arguments);
+			for(var i=0; i<arguments.length; i++){
+				var theSquare = $(arguments[i]);
+				snapSquare = this.g.select('#'+theSquare.attr('id'))
+				snapSquare.addClass('type0');
+				var coords =  theSquare.attr('id').slice(6).split('-');
+				this.mapArray[coords[1]][coords[0]] = 0;
+			}
+			console.log(this.mapArray);
+		},
 		removeGrid: function(){
 			this.gridSet.clear();
+			$('#squareX').text('');
+			$('#squareY').text('');
 		}
 	}
 
 	var map = new Mapster(g, s);
-	// map.drawGrid(75);
 
   //////////////////////////////////////
  ///////// Panzoom.js Handlers ////////
@@ -75,7 +107,6 @@ function createMap(mapdata){
 	//Handles mousewheel zooming on map.
 	$mapContainer.on('mousewheel.focal', function( e ) {
 		e.preventDefault();
-		console.log(e);
 		var delta = e.delta || e.originalEvent.wheelDelta;
 		var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0;
 		$themap.panzoom('zoom', zoomOut, {
@@ -90,15 +121,13 @@ function createMap(mapdata){
 	});
 	//Ensures the control handle remains green on drag.
 	$controlHandle.on('panzoomstart', function(){
-		console.log('yup');
 		$(this).css('background-color', 'lightgreen');
 	}).on('panzoomend', function(){
 		$(this).css('background-color', '');
 	})
 	//Only show move cursor when dragging.
-	$themap.on('panzoomstart', function(){
-		console.log('yuppers');
-		$(this).css('cursor', 'move');
+	$themap.on('panzoomstart', function(e){
+		console.log(e);
 	}).on('panzoomend', function(){
 		$(this).css('cursor', '');
 	})
@@ -110,16 +139,25 @@ function createMap(mapdata){
 	$('#show-grid-button').on('click', showGrid);
 	function createGrid(){
 		var gridDimension = $('#grid-dimension-picker').val();
-		map.drawGrid(gridDimension);
+		map.createArray(gridDimension);
+		map.drawGrid();
 	}
 	function removeGrid(){
 		map.removeGrid();
 	}
 	function hideGrid(){
-		$('.grid-square').hide();
+		map.$gridSquares.hide();
 	}
 	function showGrid(){
-		$('.grid-square').show();
+		map.$gridSquares.show();
 	}
+
+	$('#map-container').on('click', '.grid-square', colorSquare)
+
+	function colorSquare(){
+		map.colorSquare(this);
+	}
+	
+
 
 }
